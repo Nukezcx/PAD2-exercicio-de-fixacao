fdf36c078b13702099e78ca5469bf4bf0a8b65b0
Nukezcx <79462683+Nukezcx@users.noreply.github.com>
2021-06-14 13:46:17 -0300

    Update lucas.js

--- lucas.js
+++ lucas.js
@@ -2,3 +2,25 @@
 function primeiraquestao(text) {
     return`Olá ${text} !`;
 }
+
+// 2 – Crie uma função que dado dois valores e uma Callback (passados como parâmetros), mostre no console a soma, subtração, multiplicação e divisão desses valores, dependendo da função Callabck.
+
+function segundaquestao(valor1,valor2,callback) {
+    console.log(callback(v1,v2));
+}
+
+segundaquestao(32,8, function(valor1,valor2){
+    return v1 + v2;
+}) 
+
+segundaquestao(20,18, function(valor1,valor2){
+    return v1 - v2;
+}) 
+
+segundaquestao(25,5, function(valor1,valor2){
+    return v1 / v2;
+}) 
+
+segundaquestao(10,2, function(valor1,valor2){
+    return v1 * v2;
+}) 
