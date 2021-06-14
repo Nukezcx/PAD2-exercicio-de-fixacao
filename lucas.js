5e4516bcd72066f79306f6e6fbadac3d31b255a2
Nukezcx <79462683+Nukezcx@users.noreply.github.com>
2021-06-14 15:36:57 -0300

    Update lucas.js

--- lucas.js
+++ lucas.js
@@ -39,3 +39,50 @@ var questao3PegaValores = questao3(12, function (v1,v2){
 })
 
 questao3PegaValores(8);
+
+//4 - Crie uma função que recebe um número (de 1 a 12) e retorne o mês correspondente como uma string. Por exemplo, se a entrada for 2, a função deverá retornar “fevereiro”, pois este é o 2° mês.
+function verificames (m){
+    switch (m) {
+        case 1: 
+            console.log('Janeiro');
+            break;
+        case 2:
+            console.log('Fevereiro');
+            break;
+        case 3:
+            console.log('Março');
+            break;
+        case 4:
+            console.log('Abril');
+                break;
+        case 5:
+            console.log('Maio');
+            break;
+        case 6:
+            console.log('Junho');
+                break;
+        case 7:
+            console.log('Julho');
+                break;
+        case 8:
+            console.log("Agosto");
+            break;
+        case 9: 
+            console.log("Setembro");
+            break;
+        case 10:
+            console.log("Outubro");
+            break;
+        case 11: 
+            console.log("Novembro");
+            break;
+        case 12: 
+            console.log("Dezembro");
+            break;
+        default:
+            console.log("você passou um valor inválido");
+            break;
+    }
+}
+
+verificames(5);
